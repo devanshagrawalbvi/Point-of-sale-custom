c3eb1f52e9db976bf1b1922be49f220de24179a0
devanshagrawalbvi <devansh.agrawal@brainvire.com>
2023-09-11 18:43:36 +0530

    Code Updated

--- static/src/js/Customer_Validation.js
+++ static/src/js/Customer_Validation.js
@@ -9,39 +9,47 @@ odoo.define("pos_order.inherit", function (require) {
       setup() {
         super.setup();
       }
-      async _onClickPay() {      
+      async _onClickPay() {
         // console.log(this.env.pos);
         if (this.env.pos.get_order().partner == null) {
           Gui.showPopup("ErrorPopup", {
             title: "Customer Error",
             body: `Select a customer.`,
           });
-        } 
-        else if (this.env.pos.get_order().location == undefined) {
+        } else if (this.env.pos.get_order().location == undefined) {
           Gui.showPopup("ErrorPopup", {
             title: "Shipping Location Error",
             body: `Select a shipping location.`,
           });
-        } 
-
-
-
-
-
-
-        
-        else if (this.env.pos.get_order().orderlines.length <= 0) {
-            // Gui.showPopup("ErrorPopup", {
-            //   title: "Product Error",
-            //   body: `Select product before placing order.`,
-            // });
-            return this.showNotification(
-              _.str.sprintf(this.env._t('Please select products before palcing order.')),
-              5000
+        } else if (this.env.pos.get_order().orderlines.length <= 0) {
+          // Gui.showPopup("ErrorPopup", {
+          //   title: "Product Error",
+          //   body: `Select product before placing order.`,
+          // });
+          return this.showNotification(
+            _.str.sprintf(
+              this.env._t("Please select products before palcing order.")
+            ),
+            3000
           );
-          }
-        else {
+        } else {
           super._onClickPay();
+        }
+      }
+
+      // Code for showing popup if product is out of stock
+      // Called when we click product 
+
+      async _clickProduct(event) {
+        if (event.detail.qty_available < 0) {
+          return this.showNotification(
+            _.str.sprintf(
+              this.env._t("Out of stock")
+            ),
+            1000
+          );
+        } else {
+          super._clickProduct(...arguments);
         }
       }
     };
